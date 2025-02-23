7ca03fbe2661b5c37e9dfbe5adbff618fcd10520
Rashi <rbihani@ucdavis.edu>
2025-02-22 19:06:34 -0800

    everypicture final

---
+++ everypicture/v2-final-version/script.js
@@ -0,0 +1,29 @@
+// --- IIFE 
+'use strict'
+console.log('reading js');
+(function (){
+    const imgs = document.querySelectorAll('.carousel-slide');
+    const overlays = document.querySelectorAll('.overlay');
+    const btns = document.querySelectorAll('button');
+    
+    //Show overlay for each image
+    imgs.forEach(function(value, index) {
+        console.log(value, index)
+
+        value.addEventListener('click', function(){
+            const correspondingOverlay = overlays[index];
+            correspondingOverlay.classList.replace("hidden", "visible");
+        });
+    });
+
+    //Return to carousel from overlay 
+    btns.forEach(function(button, index){
+        button.addEventListener('click', function(){
+            console.log('clicking ', index, ' button');
+            const correspondingOverlay = overlays[index];
+            correspondingOverlay.classList.replace("visible", "hidden");
+        });
+    });
+    
+
+})();
