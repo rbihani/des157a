8cfdd1787f6a483c716c30fb0a24ceace774c154
Rashi <rbihani@ucdavis.edu>
2025-02-24 17:15:49 -0800

    cloned animation

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -3,28 +3,56 @@
 console.log('reading js');
 
 (function (){
-    const imgs = document.querySelectorAll('.carousel-slide');
+    //copied code from ImageLeft from Canvas
+    const sliderContent = document.querySelector('.a');
+
+    // How wide is the original set of images?
+    const sliderWidth = sliderContent.offsetWidth;
+
+    // clone the set of images and assign them the class name of '.b'
+    const cloned = sliderContent.cloneNode(true);
+    cloned.className = "b";
+
+    // add the clone to the DOM
+    document.querySelector('.carousel-slide').appendChild(cloned);
+
+    //get the :root element
+    let root = document.querySelector(':root');
+
+    // set the end of the left position based on the width of the slider
+    const endLeftPos = `-${sliderWidth}px`;    
+    root.style.setProperty('--sliderwidth', endLeftPos);
+
+    //Add the animate class to start animating the slider
+    document.querySelector('.carousel-slide').classList.add("animate");
+
+    const imgs = document.querySelectorAll('.thumbnail');
+
+    console.log(imgs)
     const overlays = document.querySelectorAll('.overlay');
     const btns = document.querySelectorAll('button');
     
     //Open overlay for each image
     imgs.forEach(function(value, index) {
-        console.log(value, index)
-
         value.addEventListener('click', function(){
             const correspondingOverlay = overlays[index];
             correspondingOverlay.classList.replace("hidden", "visible");
+
+            document.querySelector('.animate').style.animationPlayState = 'paused';
         });
     });
 
     //Return to carousel from overlay 
     btns.forEach(function(button, index){
         button.addEventListener('click', function(){
-            console.log('clicking ', index, ' button');
             const correspondingOverlay = overlays[index];
             correspondingOverlay.classList.replace("visible", "hidden");
+
+            document.querySelector('.animate').style.animationPlayState = 'running';
         });
     });
+
+
     
 
 })();
